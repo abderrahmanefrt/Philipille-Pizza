4376bdaedd4016c014fa89bffd379a38a3e43799
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2025-07-19 18:07:07 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,69 @@
 
 import './App.css'
-
+  const pizzaList = [
+  {
+    name: "Margherita",
+    ingredients: "Tomato sauce, mozzarella, basil",
+    photo: "../public/buratapizza.jpg",
+    price: 450
+  },
+  {
+    name: "Pepperoni",
+    ingredients: "Tomato sauce, mozzarella, pepperoni",
+    photo: "../public/paperonni.jpg",
+    price: 550
+  },
+  {
+    name: "Vegetarian",
+    ingredients: "Tomato sauce, mozzarella, mushrooms, bell peppers, onions, olives",
+    photo: "../public/_pizza_spinaci-1.jpg",
+    price: 500
+  },
+  {
+    name: "Hawaiian",
+    ingredients: "Tomato sauce, mozzarella, ham, pineapple",
+    photo: "../public/hawaiian.jpg",
+    price: 580
+  },
+  {
+    name: "Quattro Formaggi",
+    ingredients: "Tomato sauce, mozzarella, gorgonzola, parmesan, ricotta",
+    photo: "../public/quattro-formaggi.jpg",
+    price: 620
+  },
+  {
+    name: "Diavola",
+    ingredients: "Tomato sauce, mozzarella, spicy salami, chili flakes",
+    photo: "../public/diavola.jpg",
+    price: 570
+  },
+  {
+    name: "Capricciosa",
+    ingredients: "Tomato sauce, mozzarella, ham, mushrooms, artichokes, olives",
+    photo: "../public/capricciosa.jpg",
+    price: 600
+  },
+  {
+    name: "Calzone (Folded)",
+    ingredients: "Tomato sauce, mozzarella, ham, mushrooms, ricotta (folded)",
+    photo: "../public/calzone.jpg",
+    price: 650
+  },
+  {
+    name: "BBQ Chicken",
+    ingredients: "BBQ sauce, mozzarella, chicken, red onions, cilantro",
+    photo: "../public/bbq-chicken.jpg",
+    price: 630
+  },
+  {
+    name: "Truffle",
+    ingredients: "White sauce, mozzarella, mushrooms, truffle oil, parmesan",
+    photo: "../public/truffle.jpg",
+    price: 750
+  }
+];
 function App() {
+
   return <div className='container'>
     <Header/>
     <Menu/>
@@ -15,12 +77,12 @@ return <h1 className='titre'>PhilipeVille Pizza</h1>
 function Menu(){
 return (
 
-<div>
+<ul>
 <h2 className='titremenu'>Our Menu</h2>
-<Pizza name='Pizza Margerita' ingredient='Tomato ,mozzarela, and  cheese' photo='../public/buratapizza.jpg' price={450}/>
-
-<Pizza name='Pizza Spinachi' ingredient='Tomato ,mozzarela, spinach, and ricotta cheese' photo='../public/_pizza_spinaci-1.jpg' price={600}/>
-</div>
+{ pizzaList.map((pizza, index)=> (
+  <Pizza key={index} pizzaobj={pizza}/> 
+))}
+</ul>
 )
 
 
@@ -33,12 +95,12 @@ function Footer(){
 
 function Pizza(props){
 
-  return <div className='pizza-container'>
-  <h3 className='pizza'>{props.name}</h3>
-  <img src={props.photo} alt="props.name" />
-  <p className='ing'>{props.ingredient}</p>
-  <p className='price'>{props.price +10 +' Da'}</p>
-  </div>
+  return <li className='pizza-container'>
+  <h3 className='pizza'>{props.pizzaobj.name}</h3>
+  <img src={props.pizzaobj.photo} alt="props.name" />
+  <p className='ing'>{props.pizzaobj.ingredient}</p>
+  <p className='price'>{props.pizzaobj.price +10 +' Da'}</p>
+  </li>
    
 }
 export default App
